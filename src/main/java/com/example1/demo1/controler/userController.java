22f598372e29e9df15078e3f41e1e6d048e8d0ef
Thisara C Prasanga <thisaraprasanga.dev@gmail.com>
2026-02-21 18:38:23 +0530

    fix the code errors

--- src/main/java/com/example1/demo1/controler/userController.java
+++ src/main/java/com/example1/demo1/controler/userController.java
@@ -1,7 +1,6 @@
 package com.example1.demo1.controler;
 
 import com.example1.demo1.dto.UserDTO;
-import com.example1.demo1.model.User;
 import com.example1.demo1.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,15 +14,14 @@ public class userController {
     @Autowired
     private UserService userService;
 
-
     @GetMapping("/getUsers")
-    public List<UserDTO> getAllUsers(){
+    public List<UserDTO> getAllUsers() {
 
         return userService.getAllUsers();
     }
 
     @PostMapping("/addUser")
-    public UserDTO addUser(@RequestBody UserDTO userDTO){
+    public UserDTO addUser(@RequestBody UserDTO userDTO) {
         return userService.addUser(userDTO);
     }
 }
--- src/main/java/com/example1/demo1/dto/UserDTO.java
+++ src/main/java/com/example1/demo1/dto/UserDTO.java
@@ -1,17 +1,14 @@
 package com.example1.demo1.dto;
 
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor //make constructors for all arguments
-@NoArgsConstructor //make plan constructor without Argument
+@AllArgsConstructor // make constructors for all arguments
+@NoArgsConstructor // make plan constructor without Argument
 public class UserDTO {
-
 
     private int id;
     private String name;
 }
-
