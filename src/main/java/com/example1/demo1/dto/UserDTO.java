87057195bc28a29818eb091d8d7a2de8cc0cf198
Thisara C Prasanga <thisaraprasanga.dev@gmail.com>
2026-02-21 12:28:50 +0530

    Make User DTO

--- src/main/java/com/example1/demo1/dto/UserDTO.java
+++ src/main/java/com/example1/demo1/dto/UserDTO.java
@@ -1,4 +1,17 @@
 package com.example1.demo1.dto;
 
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor //make constructors for all arguments
+@NoArgsConstructor //make plan constructor without Argument
 public class UserDTO {
+
+    @Id //make id is primary key
+    private int id;
+    private String name;
 }
+
