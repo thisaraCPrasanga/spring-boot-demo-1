f56538b0773f4d7ebab8c6dbe8d9078bd043249c
Thisara C Prasanga <thisaraprasanga.dev@gmail.com>
2026-02-21 12:34:51 +0530

    Make user model for Data base

--- src/main/java/com/example1/demo1/model/User.java
+++ src/main/java/com/example1/demo1/model/User.java
@@ -1,4 +1,19 @@
 package com.example1.demo1.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class User {
+
+    @Id
+    private int id;
+    private String name;
+
 }
