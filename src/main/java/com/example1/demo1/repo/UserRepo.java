b0ea41932c48a3185f9cb7242e57dfb8f1728842
Thisara C Prasanga <thisaraprasanga.dev@gmail.com>
2026-02-21 12:37:30 +0530

    Make user repository

--- src/main/java/com/example1/demo1/repo/UserRepo.java
+++ src/main/java/com/example1/demo1/repo/UserRepo.java
@@ -1,4 +1,12 @@
 package com.example1.demo1.repo;
 
-public interface UserRepo {
+import com.example1.demo1.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface UserRepo extends JpaRepository<User,Integer> {
+
+
 }
